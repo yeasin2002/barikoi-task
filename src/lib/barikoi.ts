c9bb71b276b38f2c2a3d54a75dc20927f4057c62
yeasin2002 <mdkawsarislam2002@gmail.com>
2026-04-25 19:12:30 +0600

    chore: removed unused script and fix import issues

--- src/lib/barikoi.ts
+++ src/lib/barikoi.ts
@@ -1,7 +1,6 @@
 import { createBarikoiClient } from 'barikoiapis'
-import { env } from '@/env'
 
 export const barikoi = createBarikoiClient({
-  apiKey: env.BARIKOI_API_KEY,
+  apiKey: process.env.BARIKOI_API_KEY!,
   timeout: 10000,
 })
